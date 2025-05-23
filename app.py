e6549357666cca5e2c5bfa6b8e0e7952d7651542
Tecuapacho-Martin <127567868+Tecuapacho-Martin@users.noreply.github.com>
2025-05-23 16:53:00 -0600

    Update app.py
    3.-Por ultimo agrege mi nombre a la ventana,, cambie el color de la ventana, y cambie el color de los botones y los anuncios

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ ventana = tk.Tk()
 ventana.title("Mi primera app grafica") #Modicamos el nombre
 ventana.geometry("400x200") #Modificamos el tamaño a 400x200
 # Aqui son todo la linea de codigo para ingresar el nombre
-etiqueta = tk.Label(ventana, text="Ingresa tu nombre:")
+etiqueta = tk.Label(ventana, text="Ingresa tu nombre:", bg = "lavender")# Agrege colores a los botones
 etiqueta.pack()
 entrada = tk.Entry(ventana)
 entrada.pack()
-boton = tk.Button(ventana, text="Mostrar saludo", command=saludar) #Cambiamos el mensaje del boton 
+boton = tk.Button(ventana, text="Mostrar saludo", command=saludar, bg = "lightcyan") #Cambiamos el mensaje del boton 
 boton.pack()
 etiqueta_resultado = tk.Label(ventana, text="")
 etiqueta_resultado.pack()
 # Aqui son todo la linea de codigo para ingresar la edad
-etiqueta2 = tk.Label(ventana, text="Ingresa tu edad")
+etiqueta2 = tk.Label(ventana, text="Ingresa tu edad", bg = "lavender")
 etiqueta2.pack()
 entrada2 = tk.Entry(ventana)
 entrada2.pack()
-boton2 = tk.Button(ventana, text="Tu edad es", command=edad)
+boton2 = tk.Button(ventana, text="Tu edad es", command=edad, bg = "lightcyan")
 boton2.pack()
 etiqueta_res = tk.Label(ventana, text="")
 etiqueta_res.pack()
+etiqueta_autor = tk.Label(ventana, text="Martin Tecuapacho Diaz") # Agrege mi nombre a la ventana
+etiqueta_autor.pack()
+ventana.config(bg ="Light blue") # Cambiamos el color de la ventana
 ventana.mainloop()
 
